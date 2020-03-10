09b1827c30f98326ae155cfbc3db81a4df8753f2
NGUYEN Olivier <Olivier.Nguyen@bordeaux-inp.fr>
2020-03-10 16:36:36 +0100

    Modified msg into mailbox

--- src/Shoot.rkt
+++ src/Shoot.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (require racket/trace)
 
-(struct actor (position msg))
+(struct actor (position mailbox))
 
 (define me (actor '(1 2) '((move 3 8) (move -2 -4))))
 
@@ -9,27 +9,18 @@
   (actor-position actor))
 
 (define (actor-send new-actor new-msg)
-  (struct-copy actor new-actor (msg (cons (actor-msg new-actor) new-msg))))
-
-(define (message actor)
-  (car (actor-msg actor)))
-
-(define (message-x msg)
-  (car (cdr msg)))
-
-(define (message-y msg)
-  (car (cdr (cdr msg))))
+  (struct-copy actor new-actor (mailbox (cons (actor-mailbox new-actor) new-msg))))
 
 (define (update-position new-actor)
   (struct-copy actor new-actor (position (list
-                                          (+ (car (actor-position new-actor)) (cadar (actor-msg new-actor)))
-                                          (+ (cadr (actor-position new-actor)) (caddar (actor-msg new-actor)))))
-               (msg (cdr (actor-msg new-actor)))))
+                                          (+ (car (actor-position new-actor)) (cadar (actor-mailbox new-actor)))
+                                          (+ (cadr (actor-position new-actor)) (caddar (actor-mailbox new-actor)))))
+               (mailbox (cdr (actor-mailbox new-actor)))))
 
 (define (actor-update new-actor)
   (cond
-    ((empty? (actor-msg new-actor)) new-actor)
-    ((equal? 'move (caar (actor-msg new-actor))) (actor-update (update-position new-actor)))))
+    ((empty? (actor-mailbox new-actor)) new-actor)
+    ((equal? 'move (caar (actor-mailbox new-actor))) (actor-update (update-position new-actor)))))
     
 (trace actor-update)
 (trace actor-location)

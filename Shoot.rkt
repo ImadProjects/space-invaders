ff41c6a4093edab9cfa68cc839620f3db07e12cf
NGUYEN Olivier <Olivier.Nguyen@bordeaux-inp.fr>
2020-03-10 16:12:31 +0100

    Fixed actor-update

--- Shoot.rkt
+++ Shoot.rkt
@@ -1,8 +1,9 @@
 #lang racket
+(require racket/trace)
 
 (struct actor (position msg))
 
-(define me (actor '(1 2) '(move 1 2)))
+(define me (actor '(1 2) '((move 3 8) (move -2 -4))))
 
 (define (actor-location actor)
   (actor-position actor))
@@ -21,13 +22,14 @@
 
 (define (update-position new-actor)
   (struct-copy actor new-actor (position (list
-                                          (+ (car (actor-position new-actor)) (cadr (actor-msg new-actor)))
-                                          (+ (cadr (actor-position new-actor)) (caddr (actor-msg new-actor)))))
+                                          (+ (car (actor-position new-actor)) (cadar (actor-msg new-actor)))
+                                          (+ (cadr (actor-position new-actor)) (caddar (actor-msg new-actor)))))
                (msg (cdr (actor-msg new-actor)))))
 
 (define (actor-update new-actor)
   (cond
-    ((empty? actor-msg) new-actor)
-    ((equal? 'move (car (actor-msg new-actor))) (update-position new-actor))))
+    ((empty? (actor-msg new-actor)) new-actor)
+    ((equal? 'move (caar (actor-msg new-actor))) (actor-update (update-position new-actor)))))
     
-
+(trace actor-update)
+(trace actor-location)
